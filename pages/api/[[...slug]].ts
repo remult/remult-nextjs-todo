cf2e3fad36e688a889537d2a97507d068148e742
Noam Honig <noam.honig@gmail.com>
2022-05-22 10:15:30 +0300

    adjusted src

--- pages/api/[[...slug]].ts
+++ pages/api/[[...slug]].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import * as util from 'util';
-import { api } from '../../lib/api';
+import { api } from '../../src/server/api';
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
     await util.promisify((api as any))(_req, res);
